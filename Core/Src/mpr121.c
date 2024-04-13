3be9ce546398b62516054118fece5f509f8c64f4
Advait Iyer <advaitiyer03@gmail.com>
2024-04-12 22:55:42 -0400

    Updated thresholds code

--- Core/Src/mpr121.c
+++ Core/Src/mpr121.c
@@ -7,8 +7,8 @@
 #define RST_VAL 0x63
 #define ECR_ALL_ENABLE 0b10001100
 #define NUM_ELECS 12
-#define TOUCH_THRESHOLD 0x08
-#define RELEASE_THRESHOLD 0x0A
+#define TOUCH_THRESHOLD 0x30
+#define RELEASE_THRESHOLD 0x08
 
 extern I2C_HandleTypeDef hi2c1;
 
@@ -51,23 +51,35 @@ int mpr121_init(uint8_t addr)
 int mpr121_set_thresholds(uint8_t addr, uint8_t touch, uint8_t release)
 {
 	uint8_t thresholds[] = {touch, release};
-	for (int i = 0; i < NUM_ELECS; i += 2) {
-		if (mpr121_write(addr, 0x41 + i, thresholds, 2)) {
+	for (int i = 0; i < 2*NUM_ELECS; i += 2) {
+		// touch
+		if (mpr121_write(addr, 0x41 + i, thresholds, 1)) {
 			return -1;
 		}
+		// release
+		if (mpr121_write(addr, 0x42 + i, thresholds+1, 1)) {
+			return -1;
+		}
+
 	}
 	return 0;
 }
 
 int mpr121_read(uint8_t addr, uint8_t reg_addr, uint8_t *data, int size)
 {
-	return HAL_I2C_Mem_Read(&hi2c1, (addr << 1) | 1, reg_addr, 1, data, size, HAL_MAX_DELAY);
+	return HAL_I2C_Mem_Read(&hi2c1, (addr << 1) | 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, size, HAL_MAX_DELAY);
 }
+
+int mpr121_read_nb(uint8_t addr, uint8_t reg_addr, uint8_t *data, int size)
+{
+	return HAL_I2C_Mem_Read_IT(&hi2c1, (addr << 1) | 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, size);
+}
+
 
 int mpr121_write(uint8_t addr, uint8_t reg_addr, uint8_t *data, int size)
 {
 	I2C_HandleTypeDef *sdf = &hi2c1;
-	return HAL_I2C_Mem_Write(&hi2c1, (addr << 1), reg_addr, 1, data, size, HAL_MAX_DELAY);
+	return HAL_I2C_Mem_Write(&hi2c1, (addr << 1), reg_addr, I2C_MEMADD_SIZE_8BIT, data, size, HAL_MAX_DELAY);
 }
 
 uint16_t mpr121_read_touch_status(uint8_t addr)
@@ -75,9 +87,32 @@ uint16_t mpr121_read_touch_status(uint8_t addr)
 	uint8_t status[2] = {0};
 	if (mpr121_read(addr, TOUCH_STATUS, status, 2)) {
 		return ~0;
+
 	}
 	uint16_t ret = status[1];
 	ret <<= 8;
 	ret |= status[0];
 	return ret;
+}
+
+uint16_t mpr121_read_touch_status_nb(uint8_t addr)
+{
+	uint8_t status[2] = {0};
+	if (mpr121_read_nb(addr, TOUCH_STATUS, status, 2)) {
+		return ~0;
+
+	}
+	uint16_t ret = status[1];
+	ret <<= 8;
+	ret |= status[0];
+	return ret;
+}
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+//	while (HAL_I2C_GetState(&hi2c1) != HAL_I2C_STATE_READY)
+//	{
+//	}
+	uint16_t touch_status = mpr121_read_touch_status_nb(0x5A);
 }
